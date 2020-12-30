294117cdee0bf683574ee4142a56ef32649f9375
jimmylynx <lynxjimmy@gmail.com>
2020-12-30 16:38:01 +0300

    self expiring models for storing verification codes

---
+++ models/codes.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+// user registeration codes schema
+const secretSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+    expires: 600,
+  },
+});
+
+module.exports = model("secretCode", secretSchema);
