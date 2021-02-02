e60baebd942b7d93cd8d9eeeb2f85f7a240cafba
jimmylynx <lynxjimmy@gmail.com>
2021-02-02 15:09:33 +0300

    nodenailer redo

--- config/mailer.js
+++ config/mailer.js
@@ -1,16 +1,21 @@
 require("dotenv/config");
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 class Mailer {
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-  }
+  constructor() {}
 
   mailConfig(email, name, code, codeId, host) {
     this.mailOptions = {
@@ -40,8 +45,24 @@ class Mailer {
     };
   }
 
-  sender(cb) {
-    this.transporter.sendMail(this.mailOptions, cb);
+  async sender(cb) {
+    try {
+      const accessToken = await oAuth2Client.getAccessToken();
+      this.transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.EMAIL,
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken,
+        },
+      });
+      this.transporter.sendMail(this.mailOptions, cb);
+    } catch (err) {
+      return err;
+    }
   }
 }
 
